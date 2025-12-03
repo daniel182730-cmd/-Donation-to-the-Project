4dc670bfe0feb112497e335221c7785d6ed2f3a4
daniel182730 <daniel182730@gmail.com>
2025-12-03 18:51:32 -0400

    ver cosas

--- Base de Datos/kickstarter.sql
+++ Base de Datos/kickstarter.sql
@@ -127,7 +127,9 @@ ADD CONSTRAINT chk_proyecto_titulo CHECK (CHAR_LENGTH(titulo) >= 3);
 /*==================================================
 PROCEDIMIENTOS ALMACENADOS
 ===================================================*/
--- Usuarios
+/*------- 
+Usuarios
+--------*/
 DELIMITER $$ 
 -- insertar usuario
 create procedure insertar_usuario(
@@ -186,8 +188,245 @@ begin
 end $$
 DELIMITER $$ 
 
--- Proyectos 
+/*------- 
+Proyectos
+--------*/ 
+-- insertar proyectos
+DELIMITER $$ 
+create procedure insertar_proyecto (
+in p_id_user int,
+in p_id_Categoria int,
+in p_titulo char(50),
+in p_descripcion char(250),
+in p_imagenes varchar (200),
+in p_estatus char(100),
+in p_cantidad_donaciones int,
+in p_monto_total decimal(10,2),
+in p_cantidad_comentarios int
+)
+begin 
+	insert into Proyectos(ID_User,ID_Categoria,titulo,descripcion,imagenes,estatus,cantidad_donaciones,monto_total,cantidad_comentarios)
+	values(p_id_user,p_id_Categoria,p_titulo,p_descripcion,p_imagenes,p_estatus,p_cantidad_donaciones,p_monto_total,p_cantidad_comentarios);
+end $$
+DELIMITER $$ 
 
-        
-		
+-- modificar proyectos
+DELIMITER $$ 
+create procedure modificar_proyecto (
+in p_id_proyecto int,
+in p_id_user int,
+in p_id_Categoria int,
+in p_titulo char(50),
+in p_descripcion char(250),
+in p_imagenes varchar (200),
+in p_estatus char(100),
+in p_cantidad_donaciones int,
+in p_monto_total decimal(10,2),
+in p_cantidad_comentarios int
+)
+begin 
+	update Proyectos set 
+   p_id_user = ID_User,
+   p_id_Categoria = ID_Categoria,
+   p_titulo = titulo,
+   p_descripcion= descripcion,
+   p_imagenes= imagenes,
+   p_estatus= estatus,
+   p_cantidad_donaciones = cantidad_donaciones ,
+   p_monto_total = monto_total ,
+   p_cantidad_comentarios= cantidad_comentarios 
+   
+   where ID_Proyecto = p_id_proyecto;
+    
+end $$
+DELIMITER $$ 
+
+-- eliminar proyectos
+DELIMITER $$ 
+create procedure eliminar_proyecto (
+in p_id_proyecto int
+)
+begin 
+	delete from Proyectos where ID_Proyecto = p_id_proyecto;
+end $$
+DELIMITER $$ 
+
+-- obtener proyectos
+DELIMITER $$ 
+create procedure obtener_proyectos (
+in p_id_proyecto int
+)
+begin 
+	select * from  Proyectos where ID_Proyecto = p_id_proyecto;
+end $$
+DELIMITER $$ 
+
+/*------- 
+Donaciones
+--------*/ 
+-- insertar donaciones
+DELIMITER $$ 
+create procedure insertar_donaciones(
+in p_id_user int,
+in p_id_proyecto int,
+in p_fecha_donaciones datetime,
+in p_estatus char(40),
+in p_monto decimal(10,2)
+)
+begin 
+	insert into Proyectos(ID_User,ID_Proyecto,Fecha_Donaciones,estatus,Monto)
+	values(p_id_user,p_id_proyecto,p_fecha_donaciones,p_estatus,p_monto);
+end $$
+DELIMITER $$ 
+
+-- modificar donaciones
+DELIMITER $$ 
+create procedure modificar_donaciones (
+in p_id_donaciones int,
+in p_id_user int,
+in p_id_proyecto int,
+in p_fecha_donaciones datetime,
+in p_estatus char(40),
+in p_monto decimal(10,2)
+)
+begin 
+	update Donaciones set 
+	p_id_user = ID_User,
+	p_id_proyecto = ID_Proyecto,
+	p_fecha_donaciones = Fecha_Donaciones,
+	p_estatus = estatus,
+	p_monto = Monto
+   
+   where ID_Donaciones = p_id_donaciones;
+    
+end $$
+DELIMITER $$ 
+
+-- eliminar donaciones
+DELIMITER $$ 
+create procedure eliminar_donaciones (
+in p_id_donaciones int
+)
+begin 
+	delete from Donaciones where ID_Donaciones = p_id_donaciones;
+end $$
+DELIMITER $$ 
+
+-- obtener donaciones
+DELIMITER $$ 
+create procedure obtener_donaciones (
+in p_id_donaciones int
+)
+begin 
+	select * from  Donaciones where ID_Donaciones = p_id_donaciones;
+end $$
+DELIMITER $$ 
+
+/*------- 
+Categorias 
+--------*/ 
+-- insertar categorias
+DELIMITER $$ 
+create procedure insertar_categorias(
+in p_nombre char(10),
+in p_descriopcion char (250)
+)
+begin 
+	insert into categorias(nombre,descriopcion)
+	values(p_nombre,p_descriopcion);
+end $$
+DELIMITER $$ 
+
+-- modificar categorias
+DELIMITER $$ 
+create procedure modificar_categorias (
+in p_id_categorias int,
+in p_nombre char(10),
+in p_descriopcion char (250)
+)
+begin 
+	update categorias set 
+	p_nombre = nombre,
+   p_descriopcion = descriopcion
+   where ID_Categorias = p_id_categorias ;
+    
+end $$
+DELIMITER $$ 
+
+-- eliminar categorias
+DELIMITER $$ 
+create procedure eliminar_categorias (
+in p_id_categorias int
+)
+begin 
+	delete from categorias where ID_Categorias = p_id_categorias;
+end $$
+DELIMITER $$ 
+
+-- obtener categorias 
+DELIMITER $$ 
+create procedure obtener_categorias (
+in p_id_categorias int
+)
+begin 
+	select * from categorias where ID_Categorias = p_id_categorias;
+end $$
+DELIMITER $$ 
+
+/*------- 
+Comentarios
+--------*/ 
+-- insertar comentarios
+DELIMITER $$ 
+create procedure insertar_comentario(
+in p_id_User int,	
+in p_id_Proyecto int,
+in p_comentario varchar(200),
+in p_fecha_comentario datetime
+)
+begin 
+	insert into comentarios(ID_User,ID_Proyecto,comentario,fecha_comentario)
+	values(p_id_User,p_id_Proyecto,p_comentario,p_fecha_comentario);
+end $$
+DELIMITER $$ 
+
+-- modificar comentarios
+DELIMITER $$ 
+create procedure modificar_comentarios (
+in p_id_comentarios int,
+in p_id_User int,	
+in p_id_Proyecto int,
+in p_comentario varchar(200),
+in p_fecha_comentario datetime
+)
+begin 
+	update comentarios set 
+	p_id_User = ID_User,
+	p_id_Proyecto = ID_Proyecto,
+    p_comentario = comentario,
+    p_fecha_comentario = fecha_comentario
+    
+   where ID_Comentarios = p_id_comentarios;
+end $$
+DELIMITER $$ 
+
+-- eliminar comentarios 
+DELIMITER $$ 
+create procedure eliminar_comentarios (
+in p_id_comentarios int
+)
+begin 
+	delete from comentarios where ID_Comentarios = p_id_comentarios;
+end $$
+DELIMITER $$ 
+
+-- obtener comentarios
+DELIMITER $$ 
+create procedure obtener_comentarios (
+in p_id_comentarios int
+)
+begin 
+	select * from comentarios where ID_Comentarios = p_id_comentarios;
+end $$
+DELIMITER $$ 
 
