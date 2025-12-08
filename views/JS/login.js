961134f7f1881d432e578999356b45feba0610b7
daniel182730 <daniel182730@gmail.com>
2025-12-08 15:26:46 -0400

    cosas

---
+++ views/JS/login.js
@@ -0,0 +1,13 @@
+const iniciarform = document.getElementById("iniciarform")
+const registerform = document.getElementById("registerform")
+
+document.getElementById("btniniciarsaccion").addEventListener("click", () =>{
+iniciarform.style.display = "block";
+registerform.style.display ="none";
+})
+
+document.getElementById("btnregistrarse").addEventListener("click",() =>{
+iniciarform.style.display = "none";
+registerform.style.display ="block";
+
+})
