d2e2b8f360176521d0c39b4811f1eb8f440f609a
daniel182730 <daniel182730@gmail.com>
2026-01-13 21:54:24 -0400

    modificacion error

--- static/JS/login.js
+++ static/JS/login.js
@@ -2,15 +2,22 @@
 const iniciarForm = document.getElementById('iniciarform');
 const registerForm = document.getElementById('registerform');
 const btnRegistrarse = document.getElementById('btnregistrarse');
+const btnIniciar = document.getElementById('btniniciar');
 
-// Función para mostrar un div y ocultar el otro
-function mostrarRegistro() {
+// Mostrar registro y ocultar login
+btnRegistrarse.addEventListener('click', () => {
     iniciarForm.classList.remove('ver');
     iniciarForm.classList.add('nover');
 
     registerForm.classList.remove('nover');
     registerForm.classList.add('ver');
-}
+});
 
-// Evento del botón
-btnRegistrarse.addEventListener('click', mostrarRegistro);
+// Volver al login desde registro
+btnIniciar.addEventListener('click', () => {
+    registerForm.classList.remove('ver');
+    registerForm.classList.add('nover');
+
+    iniciarForm.classList.remove('nover');
+    iniciarForm.classList.add('ver');
+});
